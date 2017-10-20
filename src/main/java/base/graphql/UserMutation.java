e807486bea9500df276718032730d9acac1fc6c0
Adam Scherer <adam@annconia.com>
2017-10-20 08:19:40 -0500

    Fixing the configuration on the model mapper.

--- src/main/java/base/graphql/UserMutation.java
+++ src/main/java/base/graphql/UserMutation.java
@@ -3,8 +3,11 @@ package base.graphql;
 import base.model.User;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author Adam Scherer
@@ -14,6 +17,12 @@ import org.springframework.stereotype.Component;
 public class UserMutation implements GraphQLMutationResolver {
 
     private static final ModelMapper modelMapper = new ModelMapper();
+
+    @PostConstruct
+    public void init() {
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+    }
 
     @Autowired
     private base.repository.UserRepository userRepository;
